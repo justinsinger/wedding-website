13985c0340d3912c8b836b88bd5cd262d1112c63
Justin Singer <just@fb.com>
2020-12-21 16:13:19 -0800

    Add to calendar

--- js/scripts.js
+++ js/scripts.js
@@ -159,23 +159,23 @@ $(document).ready(function () {
         },
         data: {
             // Event title
-            title: "Ram and Antara's Wedding",
+            title: "Justin and Karina's Wedding",
 
             // Event start date
-            start: new Date('Nov 27, 2017 10:00'),
+            start: new Date('Sept 05, 2021 5:30 PM'),
 
             // Event duration (IN MINUTES)
             // duration: 120,
 
             // You can also choose to set an end time
             // If an end time is set, this will take precedence over duration
-            end: new Date('Nov 29, 2017 00:00'),
+            end: new Date('Sept 06, 2021 12:00 AM'),
 
             // Event Address
-            address: 'ITC Fortune Park Hotel, Kolkata',
+            address: '401 S Christopher Columbus Blvd, Philadelphia, PA 19106',
 
             // Event Description
-            description: "We can't wait to see you on our big day. For any queries or issues, please contact Mr. Amit Roy at +91 9876543210."
+            description: ""
         }
     });
 
